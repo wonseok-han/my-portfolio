24d56aeca4600ab93fa87b96d74267f1bed2b812
wonseok-han <oshan1112@gmail.com>
2023-03-23 01:52:26 +0900

    basename 추가

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
   });
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
         <div
           className={`flex flex-col ${
