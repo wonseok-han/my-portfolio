1082641e01d8984648fa1d111bc90ba8d978a1cf
wonseok-han <oshan1112@gmail.com>
2023-05-23 12:11:53 +0900

    career 형태를 회사별로 보여주도록 수정.

--- src/pages/Career.tsx
+++ src/pages/Career.tsx
@@ -1,6 +1,6 @@
-import { careerData, CareerItem } from 'data/careerData';
+import { careerData, CareerItem, CareerCompany } from 'data/careerData';
 
-export default function Career() {
+const Career = () => {
   return (
     <div className="h-full bg-gray-100">
       <div className="mx-auto max-w-full py-16 px-4 sm:px-6 lg:px-8">
@@ -8,24 +8,44 @@ export default function Career() {
           Career
         </div>
         <hr className="mt-2 h-1 bg-gray-700" />
-        <ul className="mt-4 space-y-4">
-          {careerData.map((item: CareerItem) => (
-            <li
-              key={item.key}
-              className="rounded-md bg-white p-4 shadow-md"
-              data-aos="fade-up"
-              data-aos-duration="1300"
-            >
-              <h2 className="text-lg font-bold text-gray-900">{item.name}</h2>
-              <p className="mt-2 text-gray-700">소속: {item.company}</p>
-              <br />
-              <p className="mt-1 text-sm text-gray-600">기간: {item.term}</p>
-              <p className="mt-1 text-sm text-gray-600">담당: {item.role}</p>
-              <p className="mt-1 text-sm text-gray-600">기술: {item.skills}</p>
-            </li>
-          ))}
-        </ul>
+        {careerData.map((company: CareerCompany) => (
+          <div
+            key={company.key}
+            className="mt-4 space-y-4 rounded-md bg-white p-4 shadow-md"
+            data-aos="fade-up"
+            data-aos-duration="1300"
+          >
+            <h2 className="text-lg font-bold text-gray-900">
+              {company.name} ({company.term})
+            </h2>
+            <div className="m-1 rounded-md bg-gray-200">
+              {company.projects.map((item: CareerItem) => (
+                <div
+                  key={item.key}
+                  className="mb-1 ml-2 p-3 shadow-sm"
+                  data-aos="fade-up"
+                  data-aos-duration="1300"
+                >
+                  <h2 className="text-sm font-bold text-gray-900">
+                    {item.name}
+                  </h2>
+                  <p className="mt-1 text-sm text-gray-600">
+                    기간: {item.term}
+                  </p>
+                  <p className="mt-1 text-sm text-gray-600">
+                    담당: {item.role}
+                  </p>
+                  <p className="mt-1 text-sm text-gray-600">
+                    기술: {item.skills}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
-}
+};
+
+export default Career;
