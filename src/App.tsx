1ab0ea4e2a031a03ce309b3cca3fb843574ce27b
wonseok-han <oshan1112@gmail.com>
2023-09-05 21:16:16 +0900

    baseUrl, homepage 제거

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
   }, []);
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <ThemeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
         <div
           className={`flex min-h-full flex-col ${
