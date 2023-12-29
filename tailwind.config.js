f30a8b495607841cc06e2d58c435482ae32f9286
wonseok-han <oshan1112@gmail.com>
2023-12-29 10:43:12 +0900

    tailwind 글꼴 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,28 @@ module.exports = {
         },
       },
     },
+    fontFamily: {
+      sans: ['Helvetica', 'Arial', 'sans-serif'],
+      serif: [
+        'ui-sans-serif',
+        'system-ui',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+      mono: [
+        'ui-monospace',
+        'SFMono-Regular',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace',
+      ],
+    },
   },
   variants: {
     extend: {},
