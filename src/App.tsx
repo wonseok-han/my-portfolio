21f18808926c430f17d6657a2a99457cad3f0a9b
wonseok-han <oshan1112@gmail.com>
2023-12-29 10:41:54 +0900

    ScrollToTop 추가

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {
 } from 'react-query';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import ScrollToTop from 'components/ScrollToTop';
 import { ThemeContext } from 'contexts/ThemeContext';
 import Content from 'layout/Content';
 import Footer from 'layout/Footer';
@@ -58,6 +59,7 @@ const App: React.FC = () => {
                     )}
                     onReset={reset}
                   >
+                    <ScrollToTop />
                     <MyRoutes />
                   </ErrorBoundary>
                 )}
